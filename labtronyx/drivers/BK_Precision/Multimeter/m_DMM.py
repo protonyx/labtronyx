2bcd025c24afd3103e0c2c553e683a62d3d16f89
protonyx <protonyx@users.noreply.github.com>
2015-03-23 22:32:13 -0600

    Finished BK Multimeter driver

--- labtronyx/drivers/BK_Precision/Multimeter/m_DMM.py
+++ labtronyx/drivers/BK_Precision/Multimeter/m_DMM.py
@@ -32,13 +32,26 @@ class m_DMM(Base_Driver):
                                          '5492B Digital Multimeter']
     }
     
+    modes = {
+        'AC Voltage': 'VOLT:AC',
+        'DC Voltage': 'VOLT:DC',
+        'Resistance': 'RES',
+        'AC Current': 'CURR:AC',
+        'DC Current': 'CURR:DC',
+        'Frequency': 'FREQ',
+        'Period': 'PER',
+        'Diode': 'DIOD',
+        'Continuity': 'CONT' }
+    
     def _onLoad(self):
         self.instr = self.getResource()
+        
+        self.func = self.getFunction()
     
     def _onUnload(self):
         pass
     
-    def _BK_ask(self, command):
+    def query(self, command):
         resp = str(self.instr.ask(command))
         self.logger.debug(resp)
         
@@ -51,78 +64,233 @@ class m_DMM(Base_Driver):
         return resp
     
     def reset(self):
+        """
+        Reset the instrument
+        """
         self.instr.write("*RST")
         
     def getError(self):
-        resp = self._BK_ask(":SYST:ERR?")
-        return resp
+        """
+        Gets the last error message from the device.
+        
+        :returns: str
+        """
+        return self.query(":SYS:ERR?")
+        
+    def enableFrontPanel(self):
+        """
+        Enables the front panel display if it was previously disabled.
+        """
+        self.instr.write(":DISP:ENAB 1")
+        
+    def disableFrontPanel(self):
+        """
+        Disables the front panel display. Display can be re-enabled by calling
+        `enableFrontPanel` or pressing the `LOCAL` button on the instrument.
+        
+        .. note:
+        
+           When the front panel is disabled, the instrument runs faster
+        
+        """
+        self.instr.write(":DISP:ENAB 0")
 
-    def getFunction(self):
-        return self._BK_ask("FUNC?")
-
-    def setFunction(self, func):
-        self.func = func
-        self.instr.write(":FUNC %s" % self.func)
+    def setMode(self, func):
+        """
+        Set the configuration mode
+        
+        Valid modes:
+         
+          * 'AC Voltage'
+          * 'DC Voltage'
+          * 'Resistance'
+          * 'AC Current'
+          * 'DC Current'
+          * 'Frequency'
+          * 'Period'
+          * 'Diode'
+          * 'Continuity'
+          
+        :param func: Configuration mode
+        :type func: str
+        """
+        if func in self.modes:
+            value = self.modes.get(func)
+            self.instr.write(":FUNC %s" % self.value)
+            
+            self.func = self.getMode()
+        else:
+            raise RuntimeError("Invalid Function")
+        
+    def getMode(self):
+        return self.query("FUNC?")
     
-    def setFunction_DC_Voltage(self):
-        self.setFunction("VOLT:DC")
+    def setFunction(self, value):
+        """
+        Alias for `setMode`
+        """
+        return self.setMode(value)
+    
+    def getFunction(self):
+        """
+        Alias for `getMode`
+        """
+        return self.getMode()
+    def setRange(self, value):
+        """
+        Set the range for the measurement. The range is selected by specifying
+        the expected reading as an absolute value. The instrument will then
+        go to the most ideal range that will accomodate the expected reading
         
-    def setFunction_AC_Voltage(self):
-        self.setFunction("VOLT:AC")
+        Possible values for `value`:
         
-    def setFunction_DC_Current(self):
-        self.setFunction("CURR:DC")
+           * 'MIN' or '0'
+           * Number (see ranges below)
+           * 'DEFAULT'
+           * 'MAX'
+           * 'AUTO'
+           
+        Expected value ranges:
         
-    def setFunction_AC_Current(self):
-        self.setFunction("CURR:AC")
+           * DC Voltage: 0 to 1000 Volts
+           * AC Voltage: 0 to 750 Volts
+           * Current: 0 to 20 Amps
+           * Resistance: 0 to 20e6 ohms
+           * Frequency or Period: 0 to 1010 Volts
         
-    def setFunction_Resistance(self):
-        self.setFunction("RES")
-        
-    def setFunction_Frequency(self):
-        self.setFunction("FREQ")
-        
-    def setFunction_Period(self):
-        self.setFunction("PER")
-        
-    def setFunction_Diode(self):
-        self.setFunction("DIOD")
-        
-    def setFunction_Continuity(self):
-        self.setFunction("CONT")
+        :param value: Measurement Range
+        :type value: str
+        """
+        if self.func in ['freq', 'per']:
+            self.instr.write(":%s:THR:VOLT:RANG %s" % (self.func, value))
+        else:
+            if value in ['auto', 'AUTO']:
+                self.instr.write(":%s:RANG:AUTO ON" % self.func)
+            else:
+                self.instr.write(":%s:RANG:AUTO OFF" % self.func)
+                self.instr.write(":%s:RANG %s" % (self.func, str(value)))
 
     def getRange(self):
-        cmd = self.func + ":RANG?"
-        range_raw = self._BK_ask(cmd)
-        self.logger.debug("Range: %s" % str(range_raw))
+        """
+        Get the range for the measurement.
         
-    def setRange_Manual(self):
-        cmd = self.func + ":RANG:AUTO OFF"
-        self.logger.debug(cmd)
-        self.instr.write(cmd)
+        :returns: float
+        """
+        return float(self.query(self.func + ":RANG?"))
+    
+    def setIntegrationRate(self, value):
+        """
+        Set the integration period (measurement speed) for the basic measurement
+        functions (except frequency and period). Expressed as a factor of the 
+        power line frequency.
         
-    def setRange_Auto(self):
-        cmd = self.func + ":RANG:AUTO ON"
-        self.logger.debug(cmd)
-        self.instr.write(cmd)
-
-    def setRange(self, new_range):
-        cmd = self.func + ":RANG " + str(new_range)
+        .. note:
+           
+           A rate of 1 would result in 16.67 ms integration period (Assuming
+           60 hz power line frequency.
+           
+        :param value: Integration rate
+        :type value: 
+        """
+        self.instr.write(":%s:NPLC %s" % (self.func, str(value)))
+    
+    def getIntegrationRate(self):
+        """
+        Get the integration period (measurement speed). Expressed as a factor
+        of the power line frequency.
         
-        self.logger.debug(cmd)
-        self.instr.write(cmd)
+        :returns: float
+        """
+        return float(self.query(":%s:NPLC?" % (self.func)))
+    
+    def setMeasurementOffset(self, value):
+        """
+        Establish a reference value for the measurement. When the offset is
+        set, the result of a measurement will be the algebraic difference
+        between the reference value and the current reading.
+        
+        Expected value ranges:
+        
+           * DC Voltage: 0 to 1000 Volts
+           * AC Voltage: 0 to 750 Volts
+           * Current: 0 to 20 Amps
+           * Resistance: 0 to 20e6 ohms
+           
+        :param value: Reference value
+        :type value: float
+        """
+        if value is None:
+            self.instr.write(":%s:REF:ACQ" % self.func)
+        elif float(value) == 0.0:
+            self.instr.write(":%s:REF:STATE OFF" % self.func)
+        else:
+            self.instr.write(":%s:REF %s" % (self.func, str(value)))
+            
+    def getMeasurementOffset(self):
+        """
+        Get the current measurement offset value.
+        
+        :returns: float
+        """
+        return float(self.query(":%s:REF?" % self.func))
+    
+    def zeroMeasurement(self):
+        """
+        Reads the current measurement and set it as the offset.
+        """
+        self.setMeasurementOffset()
+        
+    def setTriggerSource(self, value):
+        """
+        Set the trigger source for a measurement.
+        
+        Valid values:
+        
+          * `IMMEDIATE`: Internal continuous trigger
+          * `BUS`: Triggered via USB/RS-232 Interface
+          * `MANUAL`: Triggered via the `Trig` button on the front panel
+          
+        :param value: Trigger source
+        :type value: str
+        """
+        validSources = ['IMMEDIATE', 'BUS', 'MANUAL']
+        
+        if value in validSources:
+            self.instr.write(":TRIG:SOUR %s" % value)
+        else:
+            raise ValueError('Invalid trigger source')
+    
+    def getTriggerSource(self):
+        """
+        Get the trigger source for a measurement.
+        
+        Valid values:
+        
+          * `IMMEDIATE`: Internal continuous trigger
+          * `BUS`: Triggered via USB/RS-232 Interface
+          * `MANUAL`: Triggered via the `Trig` button on the front panel
+          
+        :returns: str
+        """
+        return str(self.query("TRIG:SOUR?"))
+    
+    def trigger(self):
+        """
+        Triggers the measurement if trigger source is set to `BUS`
+        """
+        self.instr.write("*TRG")
         
     def getMeasurement(self):
+        """
+        Get the last available reading from the instrument. This command does
+        not trigger a measurement if trigger source is not set to `IMMEDIATE`.
+        
+        :returns: float
+        """
         # Attempt three times to get a measurement
         for x in range(3):
             try:
-                meas_raw = self._BK_ask("FETC?")
-                self.logger.debug(meas_raw)
-                measure = float(meas_raw)
-                if measure > 500 or measure is None: # Arbitrary value
-                    continue
-                
-                return measure
+                return float(self.query("FETC?"))
             except ValueError:
                 # Try again
                 pass
