c2f5907a7746d136c40cdaacb3a752245804bd81
protonyx <protonyx@users.noreply.github.com>
2015-12-02 13:32:03 -0700

    Added except clause for import error on loading wx views and normalized build version

--- labtronyx/gui/wx_views/__init__.py
+++ labtronyx/gui/wx_views/__init__.py
@@ -1,7 +1,11 @@
-from .wx_base import *
+try:
+    from .wx_base import *
 
-from .wx_interfaces import *
-from .wx_resources import *
-from .wx_scripts import *
-from .wx_manager import *
-from .wx_main import *
+    from .wx_interfaces import *
+    from .wx_resources import *
+    from .wx_scripts import *
+    from .wx_manager import *
+    from .wx_main import *
+
+except ImportError:
+    pass
